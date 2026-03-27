83a51982b5ba625eab3158733bb0dffccf7996db
Shakirth Anisha <shakirthanisha@gmail.com>
2026-03-27 08:33:54 +0530

    replication safety

--- replica/recovery/__init__.py
+++ replica/recovery/__init__.py
@@ -1,5 +1,12 @@
 from .restart_recovery import RecoveryLayer, RecoveryState
 from .reconciliation_engine import ReconciliationEngine
 from .consistency_checker import ConsistencyChecker
+from .replication_safety import ReplicationSafety
 
-__all__ = ["RecoveryLayer", "RecoveryState", "ReconciliationEngine", "ConsistencyChecker"]
+__all__ = [
+	"RecoveryLayer",
+	"RecoveryState",
+	"ReconciliationEngine",
+	"ConsistencyChecker",
+	"ReplicationSafety",
+]
